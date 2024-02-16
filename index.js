6879013db7f301fe14af981aa7bbc0315f4db4a4
SeaPickle <uraniumpjs@gmail.com>
2024-02-16 14:48:58 -0700

    Fixed bug

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // this retrives the text
+var lines = []
+function loadLinesArray(){
+  if(lines.length == 0){
+    fetch('https://raw.githubusercontent.com/SeaPickle754/SeaPickle754.github.io/main/affirmations.txt')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch the file');
+        }
+        return response.text();
+      })
+      .then(text => {
+        lines = text.split('\n');
+        // You can now use the 'lines' array as needed
+        console.log("Loaded lines array!");
+  });
+}
+}
 const textElement = document.getElementById("text");
 function getNewAffirmation(){
-    fetch('affirmations.txt')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch the file');
-      }
-      return response.text();
-    })
-    .then(text => {
-      const lines = text.split('\n');
-      // You can now use the 'lines' array as needed
-    });
+    if (lines.length == 0){
+      loadLinesArray();
+    }
+
     textElement.textContent = lines[Math.floor(Math.random() * lines.length)]
 }
 //https://codepen.io/SJF/pen/wBdpXV
